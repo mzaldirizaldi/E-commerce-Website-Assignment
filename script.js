057b545531d7c6f7340fb330b686b9d911d5979c
mzaldirizaldi <mzaldirizaldi@gmail.com>
2020-01-29 23:14:12 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 /*global alert*/
+/* eslint-env browser */
+/* eslint-disable no-unused-vars */
 
 function notify() { 'use strict'; alert('PT Indofood Sukses Makmur Tbk, better known as Indofood, is a major Indonesian company involved in the food industry. The companys headquarters are in Jakarta.'); }
 
@@ -16,6 +18,18 @@ function total() { 'use strict'; var x = document.getElementById("totalwrap"); i
 
 function plus() { 'use strict'; if (typeof (Storage) !== "undefined") { if (localStorage.clickcount) { localStorage.clickcount = Number(localStorage.clickcount) + 1; } else { localStorage.clickcount = 1; } document.getElementById("result").innerHTML = "You ordered " + localStorage.clickcount + " hoodie"; } if (localStorage.clickcount < 0) { document.getElementById("result").innerHTML = "You ordered nothing"; localStorage.clear(); } }
 
-function minus() { 'use strict'; if (typeof (Storage) !== "undefined") { if (localStorage.clickcount) { localStorage.clickcount = Number(localStorage.clickcount) - 1; } else { localStorage.clickcount = 1; } document.getElementById("result").innerHTML = "You ordered " + localStorage.clickcount + " hoodie"; } if (localStorage.clickcount < 0) { document.getElementById("result").innerHTML = "You ordered nothing"; } }
+function minus() { 'use strict'; if (typeof (Storage) !== "undefined") { if (localStorage.clickcount) { localStorage.clickcount = Number(localStorage.clickcount) - 1; } else { localStorage.clickcount = 1; } document.getElementById("result").innerHTML = "You ordered " + localStorage.clickcount + " hoodie"; } else { document.getElementById("result").innerHTML = "You ordered nothing"; } }
 
 function confirm() { 'use strict'; var x = document.getElementById("hoodie"); if (x.style.display === "block") { x.style.display = "none"; } else { x.style.display = "block"; } document.getElementById("result").innerHTML = "You ordered " + localStorage.clickcount + " hoodie"; }
+
+var i = 0;
+var images = [];
+var time = 3000;
+
+images[0] = 'img/image1.jpg';
+images[1] = 'img/image2.jpg';
+images[2] = 'img/image3.jpg';
+
+function changeImg() { 'use strict'; document.getElementById('frame1').src = images[i]; if (i < images.length - 1) { i += 1; } else { i = 0; } setTimeout("changeImg()", time); }
+
+window.onload = changeImg;

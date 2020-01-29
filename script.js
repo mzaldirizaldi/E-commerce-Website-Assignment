0830d471f72626d48ed4791cc45bf574ca811447
mzaldirizaldi <mzaldirizaldi@gmail.com>
2020-01-29 19:41:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,94 +1,21 @@
-function notify()
-{
-    alert('PT Indofood Sukses Makmur Tbk, better known as Indofood, is a major Indonesian company involved in the food industry. The companys headquarters are in Jakarta.')
-}
+/*global alert*/
 
-function loginpage()
-{
-  var x = document.getElementById("loginwrap");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+function notify() { 'use strict'; alert('PT Indofood Sukses Makmur Tbk, better known as Indofood, is a major Indonesian company involved in the food industry. The companys headquarters are in Jakarta.'); }
 
-function category()
-{
-  var x = document.getElementById("categorywrap");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+function loginpage() { 'use strict'; var x = document.getElementById("loginwrap"); if (x.style.display === "block") { x.style.display = "none"; } else { x.style.display = "block"; } }
 
-function hoodie()
-{
-      var x = document.getElementById("hoodie");
-    localStorage.clear();
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-       document.getElementById("result").innerHTML = "You ordered " + localStorage.clickcount + " hoodie";
-}
-function cancel()
-{
-      var x = document.getElementById("hoodie");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-    localStorage.clear()
-document.getElementById("result").innerHTML = "You ordered nothing";
-}
+function category() { 'use strict'; var x = document.getElementById("categorywrap"); if (x.style.display === "block") { x.style.display = "none"; } else { x.style.display = "block"; } }
 
-function total()
-{
-    var x = document.getElementById("totalwrap");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-function plus() {
-  if (typeof(Storage) !== "undefined") {
-    if (localStorage.clickcount) {
-      localStorage.clickcount = Number(localStorage.clickcount)+1;
-    } else {
-      localStorage.clickcount = 1;
-    }
-    document.getElementById("result").innerHTML = "You ordered " + localStorage.clickcount + " hoodie";
-  }
-    if(localStorage.clickcount < 0) {
-    document.getElementById("result").innerHTML = "You ordered nothing";
-    localStorage.clear();
-  }
-}
-function minus() {
-  if (typeof(Storage) !== "undefined") {
-    if (localStorage.clickcount) {
-      localStorage.clickcount = Number(localStorage.clickcount)-1;
-    } else {
-      localStorage.clickcount = 1;
-    }
-    document.getElementById("result").innerHTML = "You ordered " + localStorage.clickcount + " hoodie";
-  } if(localStorage.clickcount < 0) {
-    document.getElementById("result").innerHTML = "You ordered nothing";
-  }
-}
+function hoodie() { 'use strict'; var x = document.getElementById("hoodie"); localStorage.clear(); if (x.style.display === "block") { x.style.display = "none"; } else { x.style.display = "block"; }
+                   document.getElementById("result").innerHTML = "You ordered " + localStorage.clickcount + " hoodie"; }
 
-function confirm()
-{
-    var x = document.getElementById("hoodie");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-    document.getElementById("result").innerHTML = "You ordered " + localStorage.clickcount + " hoodie";
-}
+function cancel() { 'use strict'; var x = document.getElementById("hoodie"); if (x.style.display === "block") { x.style.display = "none"; } else { x.style.display = "block"; } localStorage.clear();
+                   document.getElementById("result").innerHTML = "You ordered nothing"; }
+
+function total() { 'use strict'; var x = document.getElementById("totalwrap"); if (x.style.display === "none") { x.style.display = "block"; } else { x.style.display = "none"; } }
+
+function plus() { 'use strict'; if (typeof (Storage) !== "undefined") { if (localStorage.clickcount) { localStorage.clickcount = Number(localStorage.clickcount) + 1; } else { localStorage.clickcount = 1; } document.getElementById("result").innerHTML = "You ordered " + localStorage.clickcount + " hoodie"; } if (localStorage.clickcount < 0) { document.getElementById("result").innerHTML = "You ordered nothing"; localStorage.clear(); } }
+
+function minus() { 'use strict'; if (typeof (Storage) !== "undefined") { if (localStorage.clickcount) { localStorage.clickcount = Number(localStorage.clickcount) - 1; } else { localStorage.clickcount = 1; } document.getElementById("result").innerHTML = "You ordered " + localStorage.clickcount + " hoodie"; } if (localStorage.clickcount < 0) { document.getElementById("result").innerHTML = "You ordered nothing"; } }
+
+function confirm() { 'use strict'; var x = document.getElementById("hoodie"); if (x.style.display === "block") { x.style.display = "none"; } else { x.style.display = "block"; } document.getElementById("result").innerHTML = "You ordered " + localStorage.clickcount + " hoodie"; }
